d3e11b75e8bba04f3c82dba8286194233b4e0da8
Rodión Romanovich Tabares Correa <37970518+CoKeFish@users.noreply.github.com>
2025-11-12 18:49:16 -0500

    Refactor save system namespaces and improve XML documentation formatting

--- SaveRepositoryGeneric.cs
+++ SaveRepositoryGeneric.cs
@@ -1,8 +1,8 @@
-﻿namespace Marmary.HellmenRaaun.Application.Save
+﻿namespace Marmary.SaveSystem
 {
     /// <summary>
-    /// A generic repository class for managing save data associated with a specific key.
-    /// Provides functionality to persist, load, and manage the existence of data in a save context.
+    ///     A generic repository class for managing save data associated with a specific key.
+    ///     Provides functionality to persist, load, and manage the existence of data in a save context.
     /// </summary>
     /// <typeparam name="T">The type of data to be managed by the repository.</typeparam>
     public class SaveRepositoryGeneric<T>
@@ -10,7 +10,7 @@
         #region Fields
 
         /// <summary>
-        /// Represents the context used to save, load. Contains with path and settings for the save data.
+        ///     Represents the context used to save, load. Contains with path and settings for the save data.
         /// </summary>
         private readonly ISaveContext _context;
 
@@ -29,12 +29,14 @@
         #region Properties
 
         /// <summary>
-        /// Gets or sets the value of type T managed by the repository. This property represents
-        /// the data associated with the key in the save context.
+        ///     Gets or sets the value of type T managed by the repository. This property represents
+        ///     the data associated with the key in the save context.
         /// </summary>
         public T Value { get; set; }
 
         #endregion
+
+        #region Constructors and Injected
 
         public SaveRepositoryGeneric(string key, ISaveContext context, T defaultValue = default)
         {
@@ -44,10 +46,12 @@
             Value = _context.LoadData(_key, defaultValue);
         }
 
+        #endregion
+
         #region Methods
 
         /// <summary>
-        /// Saves the current value of type <typeparamref name="T"/> to the save context using the specified key.
+        ///     Saves the current value of type <typeparamref name="T" /> to the save context using the specified key.
         /// </summary>
         public void SaveData()
         {
@@ -55,17 +59,17 @@
         }
 
         /// <summary>
-        /// Loads the value of type <typeparamref name="T"/> from the save context using the specified key.
-        /// Returns the default value if the key does not exist.
+        ///     Loads the value of type <typeparamref name="T" /> from the save context using the specified key.
+        ///     Returns the default value if the key does not exist.
         /// </summary>
-        /// <returns>The loaded value of type <typeparamref name="T"/> or the default value.</returns>
+        /// <returns>The loaded value of type <typeparamref name="T" /> or the default value.</returns>
         public T LoadData()
         {
             return _context.LoadData(_key, _defaultValue);
         }
 
         /// <summary>
-        /// Checks if the key exists in the save context.
+        ///     Checks if the key exists in the save context.
         /// </summary>
         /// <returns><c>true</c> if the key exists; otherwise, <c>false</c>.</returns>
         public bool Exists()
