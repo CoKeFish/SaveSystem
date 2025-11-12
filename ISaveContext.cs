ed5913bf8ed9b61077b2af43f7575ffa8dc82ae5
Rodión Romanovich Tabares Correa <37970518+CoKeFish@users.noreply.github.com>
2025-11-11 22:58:06 -0500

    Add ISaveContext and SaveRepositoryGeneric for data management

---
+++ ISaveContext.cs
@@ -0,0 +1,46 @@
+﻿namespace Marmary.HellmenRaaun.Application.Save
+{
+    /// <summary>
+    /// Defines methods for saving and loading data using a key-value mechanism.
+    /// </summary>
+    public interface ISaveContext
+    {
+        /// <summary>
+        /// Saves a value of type <typeparamref name="T"/> with the specified key.
+        /// </summary>
+        /// <typeparam name="T">The type of the value to save.</typeparam>
+        /// <param name="key">The key to associate with the value.</param>
+        /// <param name="value">The value to save.</param>
+        void SaveData<T>(string key, T value);
+    
+        /// <summary>
+        /// Loads a value of type <typeparamref name="T"/> associated with the specified key.
+        /// Returns <paramref name="defaultValue"/> if the key does not exist.
+        /// </summary>
+        /// <typeparam name="T">The type of the value to load.</typeparam>
+        /// <param name="key">The key associated with the value.</param>
+        /// <param name="defaultValue">The default value to return if the key does not exist.</param>
+        /// <returns>The loaded value or the default value.</returns>
+        T LoadData<T>(string key, T defaultValue = default);
+    
+        /// <summary>
+        /// Checks if a value exists for the specified key.
+        /// </summary>
+        /// <param name="key">The key to check for existence.</param>
+        /// <returns>True if the key exists; otherwise, false.</returns>
+        bool KeyExists(string key);
+    }
+    
+    //TODO: Refactor this move to separate file
+    
+    /// <summary>
+    /// Represents a global save context for application-wide data.
+    /// </summary>
+    public interface IGlobalSaveContext : ISaveContext { }
+    
+    /// <summary>
+    /// Represents a player-specific save context for user data.
+    /// </summary>
+    public interface IPlayerSaveContext : ISaveContext { }
+
+}
---
+++ SaveRepositoryGeneric.cs
@@ -0,0 +1,78 @@
+﻿namespace Marmary.HellmenRaaun.Application.Save
+{
+    /// <summary>
+    /// A generic repository class for managing save data associated with a specific key.
+    /// Provides functionality to persist, load, and manage the existence of data in a save context.
+    /// </summary>
+    /// <typeparam name="T">The type of data to be managed by the repository.</typeparam>
+    public class SaveRepositoryGeneric<T>
+    {
+        #region Fields
+
+        /// <summary>
+        /// Represents the context used to save, load. Contains with path and settings for the save data.
+        /// </summary>
+        private readonly ISaveContext _context;
+
+        /// <summary>
+        ///     The key to save the data under.
+        /// </summary>
+        private readonly string _key;
+
+        /// <summary>
+        ///     The default value to return if the key does not exist.
+        /// </summary>
+        private readonly T _defaultValue;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the value of type T managed by the repository. This property represents
+        /// the data associated with the key in the save context.
+        /// </summary>
+        public T Value { get; set; }
+
+        #endregion
+
+        public SaveRepositoryGeneric(string key, ISaveContext context, T defaultValue = default)
+        {
+            _key = key;
+            _defaultValue = defaultValue;
+            _context = context;
+            Value = _context.LoadData(_key, defaultValue);
+        }
+
+        #region Methods
+
+        /// <summary>
+        /// Saves the current value of type <typeparamref name="T"/> to the save context using the specified key.
+        /// </summary>
+        public void SaveData()
+        {
+            _context.SaveData(_key, Value);
+        }
+
+        /// <summary>
+        /// Loads the value of type <typeparamref name="T"/> from the save context using the specified key.
+        /// Returns the default value if the key does not exist.
+        /// </summary>
+        /// <returns>The loaded value of type <typeparamref name="T"/> or the default value.</returns>
+        public T LoadData()
+        {
+            return _context.LoadData(_key, _defaultValue);
+        }
+
+        /// <summary>
+        /// Checks if the key exists in the save context.
+        /// </summary>
+        /// <returns><c>true</c> if the key exists; otherwise, <c>false</c>.</returns>
+        public bool Exists()
+        {
+            return _context.KeyExists(_key);
+        }
+
+        #endregion
+    }
+}
